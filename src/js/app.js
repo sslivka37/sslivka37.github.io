ad27ce5d5922a781361a13eefa09c6fdd205c64f
Your Name <you@example.com>
2019-03-09 13:47:20 -0500

    Sarah third JS commit, added slider, just need testing

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,16 @@
+
+
+  //show or hide navigation at top
+function showOrHide() {
+  var menu = document.getElementById("navMenu");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
+
+//modal button for bio information
 var modal = document.getElementById("bioModal");
 var button = document.getElementById("bioButton");
 var span = document.getElementsByClassName("close")[0];
@@ -16,6 +29,7 @@ window.onclick = function(event) {
   }
 }
 
+//collapsible project descriptions
 var collapsible = document.getElementsByClassName("collapsible");
 var i;
 
@@ -31,11 +45,70 @@ for (i = 0; i < collapsible.length; i++) {
   });
 }
 
-function showOrHide() {
-  var menu = document.getElementById("navMenu");
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
+(function() {
+
+  setUpPage();
+
+  function setUpPage() {
+ addSliderEventListeners();
   }
-}
+
+//the slider stuff
+function addSliderEventListeners() {
+
+    const sliderPrev = document.getElementById('slider-prev');
+    const sliderNext = document.getElementById('slider-next');
+    const sliderFrame = document.getElementById('slider-frame');
+    const sliderCaption = document.getElementById('slider-caption');
+
+    const slides =
+      Array.from(sliderFrame.querySelectorAll('div[data-src]'))
+        .map(div => {
+          return {
+            imageUrl: div.getAttribute('data-src'),
+            caption: div.textContent.trim(),
+          };
+        });
+
+    let sliderIndex = 0;
+    displaySliderImage();
+
+    function displaySliderImage() {
+      let { imageUrl, caption } = slides[sliderIndex];
+      sliderFrame.style.backgroundImage = `url('${imageUrl}')`;
+      let count = `(${sliderIndex+1}/${slides.length}) `;
+      sliderCaption.innerHTML = count + caption;
+      sliderCaption.classList.add('flash');
+      setTimeout(() => {
+        sliderCaption.classList.remove('flash');
+      }, 0);
+    }
+
+    function displayPrevSliderImage() {
+      sliderIndex--;
+      if (sliderIndex < 0) {
+        sliderIndex = slides.length - 1;
+      }
+      displaySliderImage();
+    }
+
+    function displayNextSliderImage() {
+      sliderIndex++;
+      if (sliderIndex === slides.length) {
+        sliderIndex = 0;
+      }
+      displaySliderImage();
+    }
+
+    sliderPrev.addEventListener('click', displayPrevSliderImage);
+    sliderNext.addEventListener('click', displayNextSliderImage);
+
+    setInterval(function() {
+      displayNextSliderImage();
+    }, 4000);
+  }
+
+
+
+
+})();
