e15bfb5f91a1dbafb7c59d88cc9720542b7d9d42
Your Name <you@example.com>
2019-04-02 18:40:47 -0400

    Sarah commit, making page public again with a few updates

--- spec/js/app.spec.js
+++ spec/js/app.spec.js
@@ -2,6 +2,23 @@ describe('button.onclick', function() {
     // Our individual tests go here
     it('should_show_bio_information', function() {
         // Actual test goes in here
-         expect(modal.style.display().toBeVisible()
+       
+        var modal = document.getElementById("bioModal");
+		var button = document.getElementById("bioButton");
+
+		button.onclick = function() {
+		modal.style.display = "block";
+		};
+
+
+
+        button.onclick();
+         expect(modal.style.display()).toEqual("block")
     });
 });
+
+//describe('addTwoNumbers' function() {
+   //it('returns \'3\' when given \'1\' and \'2\' as parameters', function() {
+       // Actual test goes in here
+ //  });
+// });
